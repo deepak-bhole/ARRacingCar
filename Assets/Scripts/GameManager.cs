bab267637cd17bbb74c8c2dcbfc3bdf2c2874c18
bholedeep <76160240+deepak-bhole@users.noreply.github.com>
2022-07-22 14:40:22 +0530

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using Deepak.Core.Singletons;
+using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField]
+    private GlobalGameSettings globalGameSettings;
+
+    public GlobalGameSettings GlobalGameSettings
+    {
+        get
+        {
+            return globalGameSettings;
+        }
+    }
+    
+    void Awake() 
+    {
+        EnhancedTouchSupport.Enable();
+    }
+}
